19ffc015bcc85c7151ced735deef6b28f095be6a
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2023-01-17 23:27:09 +0500

    workee

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 mod models;
+mod args;
+use args::EntityType;
+use args::RustflixArgs;
+use clap::Parser;
 
 fn main() {
-    println!("Hello, world!");
+    let args = RustflixArgs::parse();
+
+/*     match args.entity_type {
+        EntityType::User(user) => handle_user_command(user),
+        EntityType::Video(video) => handle_video_command(video),
+        EntityType::View(view) => handle_view_command(view),
+    };*/
 }
